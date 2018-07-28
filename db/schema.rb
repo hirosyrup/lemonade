02ba0c86547a1fb212ccb290e3e72510f8ab50eb
hiroaki Iwai <hiro_lily_da@hotmail.com>
2018-07-28 21:44:44 +0900

    DBの設定や試しに曲のモデル・コントローラを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180728122114) do
+
+  create_table "songs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
