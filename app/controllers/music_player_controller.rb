4d8923c741486e438a5da96b4e36e329b849d7d3
hiroaki Iwai <hiro_lily_da@hotmail.com>
2018-07-28 23:31:18 +0900

    曲の情報のパース用にライブラリ導入

--- app/controllers/music_player_controller.rb
+++ app/controllers/music_player_controller.rb
@@ -1,4 +1,21 @@
+require 'taglib'
+
 class MusicPlayerController < ApplicationController
   def index
+    TagLib::FileRef.open("#{Rails.public_path}/songs/nerve.m4a") do |fileref|
+      if fileref
+        tag = fileref.tag
+        tag.title   #=> "Wake Up"
+        tag.artist  #=> "Arcade Fire"
+        tag.album   #=> "Funeral"
+        tag.year    #=> 2004
+        tag.track   #=> 7
+        tag.genre   #=> "Indie Rock"
+        tag.comment #=> nil
+
+        properties = fileref.audio_properties
+        properties.length  #=> 335 (song length in seconds)
+      end
+    end  # File is automatically closed at block end
   end
 end
