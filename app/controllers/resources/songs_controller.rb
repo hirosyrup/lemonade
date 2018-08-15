b07825d83cbf01936c6281a4086bf952fcd85262
hiroaki Iwai <hiro_lily_da@hotmail.com>
2018-08-15 22:58:17 +0900

    不要なメソッド削除

--- app/controllers/resources/songs_controller.rb
+++ app/controllers/resources/songs_controller.rb
@@ -1,10 +1,4 @@
 class Resources::SongsController < ApplicationController
-  def index
-    @songs = Song.all
-
-    render json: @songs
-  end
-
   def create
     aggregation = Aggregations::Song.new
     @song = aggregation.save(params)
