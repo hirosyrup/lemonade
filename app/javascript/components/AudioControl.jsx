e5310b4be2571daeb7dea280f09ff36915bc8079
hiroaki Iwai <hiro_lily_da@hotmail.com>
2018-08-18 15:31:41 +0900

    オーディオ再生処理部分をモデルとして切り出した

--- app/javascript/components/AudioControl.jsx
+++ app/javascript/components/AudioControl.jsx
@@ -1,62 +1,22 @@
 import React from "react"
+import AudioPlayer from './../model/AudioPlayer'
 
 class AudioControl extends React.Component {
   componentDidMount() {
-    this.context = new window.AudioContext();
-    this.setupNode();
+    this.player = new AudioPlayer();
   }
 
   render() {
     return (
         <React.Fragment>
+          <div>
+          </div>
         </React.Fragment>
     );
   }
 
   play(url) {
-    if (!url) return;
-
-    this.getAudioBuffer(url)
-        .then((buffer) => {
-          this.resetSourceConnect();
-          this.source.buffer = buffer;
-          this.source.start(0);
-        })
-  }
-
-  resetSourceConnect() {
-    if (this.source) {
-      if (this.source.buffer) {
-        this.source.stop();
-      }
-      this.source.buffer = null;
-      this.source.disconnect();
-    }
-    this.source = this.context.createBufferSource();
-    this.source.connect(this.context.destination);
-  }
-
-  setupNode() {
-    this.resetSourceConnect();
-  }
-
-  getAudioBuffer(url) {
-    return new Promise((resolve) => {
-      let req = new XMLHttpRequest();
-      req.responseType = 'arraybuffer';
-      req.onreadystatechange = (() => {
-        if (req.readyState === 4) {
-          if (req.status === 0 || req.status === 200) {
-            this.context.decodeAudioData(req.response)
-                .then((buffer) => {
-                  resolve(buffer);
-                });
-          }
-        }
-      })
-      req.open('GET', url, true);
-      req.send();
-    });
+    this.player.play(url);
   }
 }
 
