9630a8bd84a4aae5cbb8791b8fbda4f06d3d8278
hiroaki Iwai <hiro_lily_da@hotmail.com>
2018-09-08 00:01:40 +0900

    デッキを2つ並べるようにした

--- app/javascript/components/System.tsx
+++ app/javascript/components/System.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
 import axios from "./AxiosDefault";
 import Deck from './Deck';
+import Grid from '@material-ui/core/Grid';
 
 interface SystemProps {
 }
@@ -24,7 +25,16 @@ class System extends React.Component<SystemProps, SystemState> {
     render() {
         return (
             <React.Fragment>
-                {this.state.uuid && <Deck uuid={this.state.uuid}/>}
+                <Grid container spacing={16}>
+                    <Grid item xs={4}>
+                        {this.state.uuid && <Deck uuid={this.state.uuid}/>}
+                    </Grid>
+                    <Grid item xs={4}>
+                    </Grid>
+                    <Grid item xs={4}>
+                        {this.state.uuid && <Deck uuid={this.state.uuid}/>}
+                    </Grid>
+                </Grid>
             </React.Fragment>
         );
     }
