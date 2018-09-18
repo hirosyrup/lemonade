eaea06130f7356b36073f551a3844139e3746afc
hiroaki Iwai <hiro_lily_da@hotmail.com>
2018-09-18 23:41:49 +0900

    マウススクロールした時にターンテーブルが回転するようにした

--- app/javascript/components/Turntable.tsx
+++ app/javascript/components/Turntable.tsx
@@ -4,15 +4,38 @@ interface TurntableProps {
 }
 
 interface TurntableState {
+    rotate: number;
 }
 
 class Turntable extends React.Component<TurntableProps, TurntableState> {
+    private readonly bindOnDrag: (e: any) => void;
+    private readonly deltaCoef: number;
+
+    constructor(props: TurntableProps) {
+        super(props);
+
+        this.bindOnDrag = this.onDrag.bind(this);
+        this.deltaCoef = 0.5;
+        this.state = {
+            rotate: 0,
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
-                <img src='assets/turntable.png' />
+                <img src={'assets/turntable.png'}
+                     className={'turntable img'}
+                     style={{transform: "rotate(" + this.state.rotate +"deg)"}}
+                     draggable={false}
+                     onWheel={this.bindOnDrag}/>
             </React.Fragment>
         );
+    }
+
+    onDrag(e: WheelEvent) {
+        e.preventDefault();
+        this.setState({rotate: this.state.rotate + e.deltaY * this.deltaCoef});
     }
 }
 
