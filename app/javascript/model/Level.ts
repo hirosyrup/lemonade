a529cc68e08af808bff2a2f11f302f781228b250
hiroaki Iwai <hiro_lily_da@hotmail.com>
2018-10-10 00:43:31 +0900

    レベルを測るサンプル数や回数等を減らして処理を軽くした

--- app/javascript/model/Level.ts
+++ app/javascript/model/Level.ts
@@ -6,6 +6,9 @@ class Level {
 
     private readonly scriptNode: ScriptProcessorNode;
     private readonly minRms: number;
+    private readonly calcSampleNum: number;
+    private readonly updateFrameNum: number;
+    private  currentFrameNum: number;
     readonly bindDidUpdatePlayStatus: ((status: AudioSourceStatus) => void);
 
     constructor(context: AudioContext) {
@@ -13,6 +16,9 @@ class Level {
         this.scriptNode.onaudioprocess = this.onAudioProcess.bind(this);
         this.selfNode = this.scriptNode;
         this.minRms = -1000.0;
+        this.calcSampleNum = 10.0;
+        this.updateFrameNum = 8;
+        this.currentFrameNum = 0;
         this.bindDidUpdatePlayStatus = this.didUpdatePlayStatus.bind(this);
     }
 
@@ -20,23 +26,27 @@ class Level {
         let inputBuffer = ev.inputBuffer;
         let outputBuffer = ev.outputBuffer;
 
-        let rms = 0.0;
-        for (let j = 0; j < inputBuffer.length; j = (j + 1) | 0) {
-            let average = 0.0;
-            for (let i = 0; i < inputBuffer.numberOfChannels; i = (i + 1) | 0) {
-                let inputData = inputBuffer.getChannelData(i);
-                average += inputData[j];
+        if (this.currentFrameNum === 0) {
+            let rms = 0.0;
+            for (let j = 0; j < this.calcSampleNum; j = (j + 1) | 0) {
+                let average = 0.0;
+                for (let i = 0; i < inputBuffer.numberOfChannels; i = (i + 1) | 0) {
+                    let inputData = inputBuffer.getChannelData(i);
+                    average += inputData[j];
+                }
+                average = average / inputBuffer.numberOfChannels;
+                rms = rms + average * average;
             }
-            average = average / inputBuffer.numberOfChannels;
-            rms = rms + average * average;
+            rms = rms / this.calcSampleNum;
+            if (rms === 0.0) {
+                rms = this.minRms;
+            } else {
+                rms = 10 * Math.log10(Math.sqrt(rms));
+            }
+            this.notifyUpdateLevel(rms);
         }
-        rms = rms / inputBuffer.length;
-        if (rms === 0.0) {
-            rms = this.minRms;
-        } else {
-            rms = 10 * Math.log10(Math.sqrt(rms));
-        }
-        this.notifyUpdateLevel(rms);
+        this.currentFrameNum += 1;
+        this.currentFrameNum %= this.updateFrameNum;
 
         for (let i = 0; i < outputBuffer.numberOfChannels; i = (i + 1) | 0) {
             let inputData = inputBuffer.getChannelData(i);
